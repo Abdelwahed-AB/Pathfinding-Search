62a805697104dae7d1ea87cb2bdde292bad0e7cb
Abdelwahed AB <58952556+Abdelwahed-AB@users.noreply.github.com>
2020-05-29 23:13:10 +0100

    Update Colors.py
    RGB was not needed, I just used a color picker which added it in front of the values.

--- Colors.py
+++ Colors.py
@@ -1,19 +1,17 @@
 from enum import Enum
-from pygame import Color
 
-RGB = Color
 
 class ColorCode(Enum):
-    WHITE = RGB(255, 255, 255)
-    LIGHT_GRAY = RGB(238, 238, 238)
-    GRAY = RGB(125, 125, 125)
-    DARK_GRAY = RGB(78, 78, 78)
-    BLACK = RGB(17, 17, 17)
-    LIGHTER_GREEN = RGB(59, 224, 56)
-    GREEN = RGB(47, 219, 54)
-    LIGHTER_RED = RGB(230, 53, 41)
-    RED = RGB(227, 42, 42)
-    BLUE = RGB(20, 94, 238)
-    ORANGE = RGB(245, 153, 58)
-    YELLOW = RGB(233, 221, 52)
-    BG_COLOR = RGB(38, 38, 38)
+    WHITE = (255, 255, 255)
+    LIGHT_GRAY = (238, 238, 238)
+    GRAY = (125, 125, 125)
+    DARK_GRAY = (78, 78, 78)
+    BLACK = (17, 17, 17)
+    LIGHTER_GREEN = (59, 224, 56)
+    GREEN = (47, 219, 54)
+    LIGHTER_RED = (230, 53, 41)
+    RED = (227, 42, 42)
+    BLUE = (20, 94, 238)
+    ORANGE = (245, 153, 58)
+    YELLOW = (233, 221, 52)
+    BG_COLOR = (38, 38, 38)
