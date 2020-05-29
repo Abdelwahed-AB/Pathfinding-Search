8e585707d5fe51eecce77b1a8d9ec4c73020825c
Abdelwahed <ababdelwahed112@gmail.com>
2020-05-29 14:17:49 +0100

    Added the basic structure

---
+++ Grid.py
@@ -0,0 +1,175 @@
+import pygame
+
+rgb = pygame.Color
+
+white = rgb(255, 255, 255)
+gray = rgb(78, 78, 78)
+black = rgb(17, 17, 17)
+green = rgb(47, 219, 54)
+red = rgb(227, 42, 42)
+blue = rgb(20, 94, 238)
+orange = rgb(245, 153, 58)
+yellow = rgb(233, 221, 52)
+
+
+class Node(object):
+    def __init__(self, Surface, x, y, width, i, j, offsetx=0, offsety=0):
+
+        self.x = x
+        self.y = y
+        self.width = width
+        self.surface = Surface
+
+        self.i = i
+        self.j = j
+
+        # For A-Star
+        self.f = 0
+        self.g = 0
+        self.h = 0
+        self.camefrom = None
+
+        self.color = black  # SET as BG color
+
+        self.offsetx = offsetx
+        self.offsety = offsety
+
+        self.neighbours = []
+
+        self.wall_neighbours = []  # For maze generation (I need to remake it)
+
+        # Node states
+        self.isStart = False
+        self.isEnd = False
+        self.isWall = False
+        self.isPath = False
+        self.isCurrent = False
+        self.checked = False
+
+    def draw(self):
+        # What is needed? : Highlight if mouseOver, Change color to white if it's a wall
+
+        # Set the state of the node
+        if isMouseOver(self):
+            # Left Mouse Button
+            if pygame.mouse.get_pressed()[0] == 1:
+                self.isWall = True
+            # Right Mouse Button
+            if pygame.mouse.get_pressed()[2] == 1:
+                self.isWall = False
+
+            self.color = gray
+        elif self.isStart:
+            self.color = yellow
+        elif self.isEnd:
+            self.color = orange
+        elif self.isWall:
+            self.color = white
+        elif self.isPath:
+            self.color = blue
+        elif self.checked:
+            self.color = red
+        elif self.isCurrent:
+            self.color = green
+        else:
+            self.color = black
+
+        # Render the node on the screen
+        rect = pygame.Rect(self.x, self.y, self.width, self.width)
+        pygame.draw.rect(self.surface, self.color, rect)
+
+    def WallNeighbours(self, arr=[]):  # WIP (Need to remake the whole classes so this is easier)
+        a = []
+        for node in self.wall_neighbours:
+            if not node.isWall and node not in arr:
+                a.append(node)
+        return(a)
+
+    def hasNeighbours(self):  # WIP (Need to remake the whole classes so this is easier)
+        a = []
+        for node in self.neighbours:
+            if not node.isWall:
+                a.append(node)
+        return(a)
+
+    def distance(self, node):
+        """Returns distance between current node and passed node"""
+        x = abs(self.j - node.j)
+        y = abs(self.i - node.i)
+        return(x + y)
+
+
+class Grid(object):
+    def __init__(self, Surface, x=0, y=0, dimensions=(100, 100), node_width=10):
+        # What is needed? : Create the nodes, Store the nodes, give each node its neighbours.
+
+        self.surface = Surface
+        self.x = x
+        self.y = y
+        self.dimensions = dimensions
+        self.n_width = node_width
+
+        self.x_elems = self.dimensions[0] // self.n_width  # Number of nodes in a row
+        self.y_elems = self.dimensions[1] // self.n_width  # Number of nodes in a column
+
+        self.grid = []
+        self.grid_surf = pygame.Surface(self.dimensions)
+
+        # Create the nodes
+        for i in range(self.y_elems):
+            a = []
+            for j in range(self.x_elems):
+                a.append(Node(self.grid_surf, j * self.n_width, i *
+                              self.n_width, self.n_width, i, j, self.x, self.y))
+            self.grid.append(a)
+        # Give each node its neighbours
+        for i in range(self.y_elems):
+            for j in range(self.x_elems):
+                if j > 0:  # Left neighbour
+                    self.grid[i][j].neighbours.append(self.grid[i][j - 1])
+                if j < self.x_elems - 1:  # Right neighbour
+                    self.grid[i][j].neighbours.append(self.grid[i][j + 1])
+                if i > 0:  # Top neighbour
+                    self.grid[i][j].neighbours.append(self.grid[i-1][j])
+                if i < self.y_elems - 1:  # Bottom neighbour
+                    self.grid[i][j].neighbours.append(self.grid[i+1][j])
+
+        # For maze generation (Kind of stupid of me, Need to remake)
+        for i in range(0, self.y_elems):
+            for j in range(0, self.x_elems):
+                if j > 1:  # Left neighbour
+                    self.grid[i][j].wall_neighbours.append(self.grid[i][j - 2])
+                if j < self.x_elems - 2:  # Right neighbour
+                    self.grid[i][j].wall_neighbours.append(self.grid[i][j + 2])
+                if i > 0:  # Top neighbour
+                    self.grid[i][j].wall_neighbours.append(self.grid[i-2][j])
+                if i < self.y_elems - 2:  # Bottom neighbour
+                    self.grid[i][j].wall_neighbours.append(self.grid[i+2][j])
+
+    def draw(self):
+        # Draw the grid
+        self.grid_surf.fill(black)  # Replace with background color
+
+        # Draw the nodes
+        for row in self.grid:
+            for node in row:
+                node.draw()
+
+        # Render to screen
+        self.surface.blit(self.grid_surf, (self.x, self.y))
+
+    def reset(self, walls):
+        for row in self.grid:
+            for node in row:
+                if walls:
+                    node.isWall = False
+                node.checked = False
+                node.isCurrent = False
+                node.isPath = False
+
+
+def isMouseOver(node):
+    if (node.x + node.offsetx < pygame.mouse.get_pos()[0] < node.x + node.offsetx + node.width) and (node.y + node.offsety < pygame.mouse.get_pos()[1] < node.y + node.width + node.offsety):
+        return(True)
+    else:
+        return(False)
---
+++ PathFinding.py
@@ -0,0 +1,409 @@
+from pygame.locals import *
+from Grid import *
+import time
+import pygame
+import sys
+from random import randint
+
+rgb = pygame.Color
+clock = pygame.time.Clock()
+# Pygame window attributes
+WINDOW_SIZE = (610, 410)
+BG_COLOR = rgb(38, 38, 38)
+FPS = 60
+
+# Default Colors
+green = rgb(59, 224, 56)
+red = rgb(230, 53, 41)
+white = rgb(238, 238, 238)
+gray = rgb(125, 125, 125)
+
+# init Pygame
+pygame.init()
+screen = pygame.display.set_mode(WINDOW_SIZE)
+# Learning about pathfinding / search algorithms
+
+g = Grid(screen, dimensions=WINDOW_SIZE)
+
+
+def BreadthFS(start=(0, 0), end=(g.x_elems - 1, g.y_elems - 1)):
+    g.reset(False)
+    end = g.grid[end[1]][end[0]]
+    # Paths to extend
+    agenda = [[g.grid[start[1]][start[0]]]]
+    # already visited / checked / extended
+    extendedList = []
+
+    while agenda:
+        path = agenda.pop(0)
+        if path[-1] == end:
+            for node in path:  # For Drawing the final path
+                node.isPath = True
+            g.draw()
+            pygame.display.update()
+            print(len(path))  # Return the path (might need it in the future)
+            return
+        elif path[-1] not in extendedList:  # Check if we visited the current node before or not
+            extendedList.append(path[-1])
+            for node in path[-1].neighbours:
+                # Check if the next node is a wall and check if it was already visited (Makes it a bit faster (Which I think is a bit weird), I tested it without the check)
+                if node not in extendedList and not node.isWall:
+                    path.append(node)
+                    agenda.append(path.copy())  # Add to the back of the que
+                    path.pop(-1)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == pygame.K_b:
+                    return
+        # Draw the current node (green) + checked nodes(red)
+        path[-1].isCurrent = True
+        g.draw()
+        path[-1].isCurrent = False
+        path[-1].checked = True
+        pygame.display.update()
+    return(None)
+
+
+def DepthFS(start=(0, 0), end=(g.x_elems - 1, g.y_elems - 1)):
+    g.reset(False)
+    """Not the best for open grids"""
+    end = g.grid[end[1]][end[0]]
+    # Paths to extend
+    agenda = [[g.grid[start[1]][start[0]]]]
+    # already visited / checked / extended
+    extendedList = []
+
+    while agenda:
+        path = agenda.pop(0)
+        if path[-1] == end:
+            for node in path:  # For Drawing the final path
+                node.isPath = True
+            g.draw()
+            pygame.display.update()
+            print(len(path))
+            return  # Return the path (might need it in the future)
+        elif path[-1] not in extendedList:  # Check if we visited the current node before or not
+            extendedList.append(path[-1])
+            for node in path[-1].neighbours:
+                # Check if the next node is a wall
+                if node not in extendedList and not node.isWall:
+                    path.append(node)
+                    agenda.insert(0, path.copy())  # Add to the front of the queue
+                    path.pop(-1)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == pygame.K_d:
+                    return
+        # Draw the current node (green) + checked nodes(red)
+        path[-1].isCurrent = True
+        g.draw()
+        path[-1].isCurrent = False
+        path[-1].checked = True
+        pygame.display.update()
+    return(None)
+
+
+def BestFS(start=(0, 0), end=(g.x_elems - 1, g.y_elems - 1)):
+    g.reset(False)
+    end = g.grid[end[1]][end[0]]
+    # Paths to extend
+    openSet = [g.grid[start[1]][start[0]]]
+    # already visited / checked / extended
+    closedSet = []
+    while openSet:
+        lowest = 0
+        for i in range(len(openSet)):
+            if openSet[i].f < openSet[lowest].f:
+                lowest = i
+
+        current = openSet[lowest]
+
+        if current == end:
+            path = []  # If we ever need it
+            tmp = current
+            tmp.isPath = True
+            path.append(tmp)
+            while tmp.camefrom != None:
+                path.append(tmp.camefrom)
+                tmp = tmp.camefrom
+                tmp.isPath = True  # Draw the path
+            g.draw()
+            pygame.display.update()
+            print(len(path))
+            return
+
+        # Mark the current node as visited / checked
+        openSet.remove(current)
+        closedSet.append(current)
+
+        # Draw the current node (green) + checked nodes(red)
+        current.isCurrent = True
+        g.draw()
+        current.isCurrent = False
+        current.checked = True
+        pygame.display.update()
+
+        for node in current.neighbours:
+            if not node.isWall and not node in closedSet:
+                if not node in openSet:
+                    openSet.append(node)
+
+                node.f = node.distance(end)
+                node.camefrom = current
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == pygame.K_v:
+                    return
+
+
+def HillClimb(start=(0, 0), end=(g.x_elems - 1, g.y_elems - 1)):
+    g.reset(False)
+    """Not the best for open grids"""
+    end = g.grid[end[1]][end[0]]
+    # Paths to extend
+    agenda = [[g.grid[start[1]][start[0]]]]
+    # already visited / checked / extended
+    extendedList = []
+
+    while agenda:
+        path = agenda.pop(0)
+        if path[-1] == end:
+            for node in path:  # For Drawing the final path
+                node.isPath = True
+            g.draw()
+            pygame.display.update()
+            print(len(path))
+            return  # Return the path (might need it in the future)
+        elif path[-1] not in extendedList:  # Check if we visited the current node before or not
+            extendedList.append(path[-1])
+            a = []  # The paths that will be added to the agenda after being sorted
+            for node in path[-1].neighbours:
+                # Check if the next node is a wall
+                if node not in extendedList and not node.isWall:
+                    path.append(node)
+                    a.append(path.copy())  # Add to a
+                    path.pop(-1)
+
+            # Sort a
+            if a:
+                min = a[0][-1].distance(end)
+                for i in range(len(a)):
+                    d = a[i][-1].distance(end)
+                    if d < min:
+                        t = a[0]
+                        a[0] = a[i]
+                        a[i] = t
+                        min = d
+                for p in a[::-1]:
+                    agenda.insert(0, p)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == pygame.K_h:
+                    return
+        # Draw the current node (green) + checked nodes(red)
+        path[-1].isCurrent = True
+        g.draw()
+        path[-1].isCurrent = False
+        path[-1].checked = True
+        pygame.display.update()
+    return(None)
+
+
+# its like BreadthFS in this context
+def BranchAndBound(start=(0, 0), end=(g.x_elems - 1, g.y_elems - 1)):
+    g.reset(False)
+    end = g.grid[end[1]][end[0]]
+    # Paths to extend
+    openSet = [g.grid[start[1]][start[0]]]
+    # already visited / checked / extended
+    closedSet = []
+    while openSet:
+        lowest = 0
+        for i in range(len(openSet)):
+            if openSet[i].f < openSet[lowest].f:
+                lowest = i
+
+        current = openSet[lowest]
+
+        if current == end:
+            path = []  # If we ever need it
+            tmp = current
+            tmp.isPath = True
+            path.append(tmp)
+            while tmp.camefrom != None:
+                path.append(tmp.camefrom)
+                tmp = tmp.camefrom
+                tmp.isPath = True  # Draw the path
+            g.draw()
+            pygame.display.update()
+            print(len(path))
+            return
+
+        # Mark the current node as visited / checked
+        openSet.remove(current)
+        closedSet.append(current)
+
+        # Draw the current node (green) + checked nodes(red)
+        current.isCurrent = True
+        g.draw()
+        current.isCurrent = False
+        current.checked = True
+        pygame.display.update()
+
+        for node in current.neighbours:
+            if not node.isWall and not node in closedSet:
+                tempG = current.g + 1
+                if node in openSet:
+                    if tempG < node.g:
+                        node.g = tempG
+                else:
+                    node.g = tempG
+                    openSet.append(node)
+
+                node.f = node.g
+                node.camefrom = current
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == pygame.K_n:
+                    return
+    return(None)
+
+
+def Astar(start=(0, 0), end=(g.x_elems - 1, g.y_elems - 1)):
+    g.reset(False)
+    end = g.grid[end[1]][end[0]]
+    # Paths to extend
+    openSet = [g.grid[start[1]][start[0]]]
+    # already visited / checked / extended
+    closedSet = []
+    while openSet:
+        lowest = 0
+        for i in range(len(openSet)):
+            if openSet[i].f < openSet[lowest].f:
+                lowest = i
+
+        current = openSet[lowest]
+
+        if current == end:
+            path = []  # If we ever need it
+            tmp = current
+            tmp.isPath = True
+            path.append(tmp)
+            while tmp.camefrom != None:
+                path.append(tmp.camefrom)
+                tmp = tmp.camefrom
+                tmp.isPath = True  # Draw the path
+            g.draw()
+            pygame.display.update()
+            print(len(path))
+            return
+
+        # Mark the current node as visited / checked
+        openSet.remove(current)
+        closedSet.append(current)
+
+        # Draw the current node (green) + checked nodes(red)
+        current.isCurrent = True
+        g.draw()
+        current.isCurrent = False
+        current.checked = True
+        pygame.display.update()
+
+        for node in current.neighbours:
+            if not node.isWall and not node in closedSet:
+                tempG = current.g + 1
+                if node in openSet:
+                    if tempG < node.g:
+                        node.g = tempG
+                else:
+                    node.g = tempG
+                    openSet.append(node)
+
+                node.h = node.distance(end)
+                node.f = node.g + node.h
+                node.camefrom = current
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == 113:
+                    return
+    return(None)
+
+
+def reset():
+    g.reset(True)
+
+
+def Maze(start=(0, 0)):
+    g.reset(True)
+    fill_Walls()
+    strt = g.grid[start[1]][start[0]]
+    visited = [strt]
+    stack = [strt]
+
+    while stack != []:
+        current = stack.pop(0)
+        if len(current.WallNeighbours(visited)) != 0:
+            stack.insert(0, current)
+            chosen = current.WallNeighbours(visited)[randint(0,
+                                                             len(current.WallNeighbours(visited))) - 1]
+            y = (current.i + chosen.i) // 2
+            x = (current.j + chosen.j) // 2
+            g.grid[y][x].isWall = False
+            visited.append(chosen)
+            stack.insert(0, chosen)
+
+
+def fill_Walls():
+    for i in range(1, g.y_elems, 2):
+        for j in range(0, g.x_elems):
+            g.grid[i][j].isWall = True
+    for i in range(1, g.x_elems, 2):
+        for j in range(0, g.y_elems):
+            g.grid[j][i].isWall = True
+
+
+funcs = {pygame.K_b: BreadthFS, pygame.K_d: DepthFS, pygame.K_g: Maze, pygame.K_v: BestFS,
+         pygame.K_h: HillClimb, 113: Astar, pygame.K_n: BranchAndBound, pygame.K_BACKSPACE: reset}
+
+start = (0, 0)
+end = (g.x_elems - 1, g.y_elems - 1)
+while True:
+    # Reset Start and end Nodes
+    g.grid[start[1]][start[0]].isStart = True
+    g.grid[start[1]][start[0]].isWall = False
+    g.grid[end[1]][end[0]].isEnd = True
+    g.grid[end[1]][end[0]].isWall = False
+
+    g.draw()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            sys.exit()
+        if event.type == KEYDOWN:
+            mousePos = pygame.mouse.get_pos()
+            if event.key in funcs.keys():
+                if not event.key in [pygame.K_BACKSPACE, pygame.K_g]:
+                    funcs[event.key](start, end)
+                else:
+                    funcs[event.key]()
+            if event.key == pygame.K_s:
+                g.grid[start[1]][start[0]].isStart = False
+                start = ((mousePos[0] // g.n_width), (mousePos[1] // g.n_width))
+            if event.key == pygame.K_e:
+                g.grid[end[1]][end[0]].isEnd = False
+                end = ((mousePos[0] // g.n_width), (mousePos[1] // g.n_width))
+
+    pygame.display.update()
